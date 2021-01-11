996f7a1b7e716d85e5089597823077070fbf9904
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2021-01-10 21:40:07 -0800

    There is a required check for empty fields when adding items to database

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -82,7 +82,7 @@ class Admin extends Component {
     //Fetch call to save item changes upon editing
     saveItemChanges() {
         if(Object.keys(this.state.insertItem).length < 10) {
-            console.log("Error! Not sufficient data in the object");
+            console.log("Error! not enough items in addItem object")
             return;
         }
 
@@ -458,129 +458,130 @@ class Admin extends Component {
                 </Modal.Header>
 
                 <Modal.Body>
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input name="name" type="text" class="form-control" id="name" placeholder="Enter name" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="name">Description</label>
-                        <input name="description" type="text" class="form-control" id="description" placeholder="Enter description" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                    </div>
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input name="name" type="text" class="form-control" id="name" placeholder="Enter name" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="name">Description</label>
+                            <input name="description" type="text" class="form-control" id="description" placeholder="Enter description" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                        </div>
 
-                    <label for="description">Category</label>
+                        <label for="description">Category</label>
 
-                    <div class="form-check">
-                        <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Appetizers" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                        <label class="form-check-label" for="Appetizers">
-                            Appetizers
-                        </label>
-                    </div>
+                        <div class="form-check">
+                            <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Appetizers" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                            <label class="form-check-label" for="Appetizers">
+                                Appetizers
+                            </label>
+                        </div>
 
-                    <div class="form-check">
-                        <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Main Dishes" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                        <label class="form-check-label" for="Main Dishes">
-                            Main Dishes
-                        </label>
-                    </div>
+                        <div class="form-check">
+                            <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Main Dishes" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                            <label class="form-check-label" for="Main Dishes">
+                                Main Dishes
+                            </label>
+                        </div>
 
-                    <div class="form-check">
-                        <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Sides" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                        <label class="form-check-label" for="Sides">
-                            Sides
-                        </label>
-                    </div>
+                        <div class="form-check">
+                            <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Sides" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                            <label class="form-check-label" for="Sides">
+                                Sides
+                            </label>
+                        </div>
 
-                    <div class="form-check">
-                        <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Drinks" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                        <label class="form-check-label" for="Drinks">
-                            Drinks
-                        </label>
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="price">Price</label>
-                        <input name="price" type="number" step="0.01" class="form-control" id="price" placeholder="Enter price" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                    </div>
+                        <div class="form-check">
+                            <input class="form-check-input" name="category" type="radio" name="category" id="category" value="Drinks" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                            <label class="form-check-label" for="Drinks">
+                                Drinks
+                            </label>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="price">Price</label>
+                            <input name="price" type="number" step="0.01" class="form-control" id="price" placeholder="Enter price" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                        </div>
 
-                    <div class="form-group">
-                        <label for="image">Image Link</label>
-                        <input name="image" type="text" class="form-control" id="image" placeholder="Enter link" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="cuisine">Cuisine</label>
-                        <input name="cuisine" type="text" class="form-control" id="cuisine" placeholder="Enter cuisine" required 
-                            onChange={(e) => this.updateInsertField(e)}
-                        />
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="ingredients">Ingredients</label>
-                        <input name="ingredients" type="text" class="form-control" id="ingredients" placeholder="Enter ingredients, separated by commas" required 
-                            onChange={(e) => this.updateInsertField(e, true)}
-                        />
-                    </div>
+                        <div class="form-group">
+                            <label for="image">Image Link</label>
+                            <input name="image" type="text" class="form-control" id="image" placeholder="Enter link" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="cuisine">Cuisine</label>
+                            <input name="cuisine" type="text" class="form-control" id="cuisine" placeholder="Enter cuisine" required 
+                                onChange={(e) => this.updateInsertField(e)}
+                            />
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="ingredients">Ingredients</label>
+                            <input name="ingredients" type="text" class="form-control" id="ingredients" placeholder="Enter ingredients, separated by commas" required 
+                                onChange={(e) => this.updateInsertField(e, true)}
+                            />
+                        </div>
 
-                    <div class="form-check form-check-inline">
-                        <input name="vegan" class="form-check-input" type="checkbox" checked={this.state.vegan} id="vegan" 
-                            onChange={(e) => this.updateInsertFieldCheckbox(e)}
-                        />
-                        <label class="form-check-label" for="defaultCheck1">
-                            Vegan
-                        </label>
-                    </div>
-                    
-                    <div class="form-check form-check-inline">
-                        <input name="vegetarian" class="form-check-input" type="checkbox" checked={this.state.vegetarian} id="vegetarian" 
-                            onChange={(e) => this.updateInsertFieldCheckbox(e)}
-                        />
-                        <label class="form-check-label" for="defaultCheck2">
-                            Vegetarian
-                        </label>
-                    </div>
-                    
-                    <div class="form-check form-check-inline">
-                        <input name="glutenFree" class="form-check-input" type="checkbox" checked={this.state.glutenFree} id="glutenFree" 
-                            onChange={(e) => this.updateInsertFieldCheckbox(e)}
-                        />
-                        <label class="form-check-label" for="defaultCheck3">
-                            Gluten Free
-                        </label>
-                    </div>
+                        <div class="form-check form-check-inline">
+                            <input name="vegan" class="form-check-input" type="checkbox" checked={this.state.vegan} id="vegan" 
+                                onChange={(e) => this.updateInsertFieldCheckbox(e)}
+                            />
+                            <label class="form-check-label" for="defaultCheck1">
+                                Vegan
+                            </label>
+                        </div>
+                        
+                        <div class="form-check form-check-inline">
+                            <input name="vegetarian" class="form-check-input" type="checkbox" checked={this.state.vegetarian} id="vegetarian" 
+                                onChange={(e) => this.updateInsertFieldCheckbox(e)}
+                            />
+                            <label class="form-check-label" for="defaultCheck2">
+                                Vegetarian
+                            </label>
+                        </div>
+                        
+                        <div class="form-check form-check-inline">
+                            <input name="glutenFree" class="form-check-input" type="checkbox" checked={this.state.glutenFree} id="glutenFree" 
+                                onChange={(e) => this.updateInsertFieldCheckbox(e)}
+                            />
+                            <label class="form-check-label" for="defaultCheck3">
+                                Gluten Free
+                            </label>
+                        </div>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={() => this.setState({
+                                insertItem: {
+                                    vegan: false,
+                                    vegetarian: false,
+                                    glutenFree: false
+                                },
+                                renderAddItems: false
+                            })}>
+                                Close
+                            </Button>
+                            <Button variant="primary" type="submit" onClick={() => this.saveItemChanges()}>
+                                Save Changes
+                            </Button>
+                        </Modal.Footer>
+                    </form>
                 </Modal.Body>
-                
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => this.setState({
-                        insertItem: {
-                            vegan: false,
-                            vegetarian: false,
-                            glutenFree: false
-                        },
-                        renderAddItems: false
-                    })}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={() => this.saveItemChanges()}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
             </Modal>
         )
     }
