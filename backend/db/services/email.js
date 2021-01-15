931db93a16a128c7d74bcd8bdf7e45b8285dd7bc
Thomas Garry <32282600+thomas-gg@users.noreply.github.com>
2021-01-14 23:41:48 -0800

    updated routes to include /change

--- backend/db/services/email.js
+++ backend/db/services/email.js
@@ -1,34 +1,50 @@
 const { Email } = require("../models/email");
 
-// if email doesn't exist return err otherwise return email
-async function addnewEmail(raw_email) {
+// change email
+async function changeEmail(raw_email) {
   try {
-    email = new Email(raw_email);
-    await email.save();
+    let email = await Email.findOneAndUpdate({}, raw_email, { new: true });
+    // if there are no emails in database insert one
+    if (email === null) {
+      email = new Email(raw_email);
+      await email.save();
+    }
     return email;
   } catch (err) {
     return false;
   }
 }
 
+// if email doesn't exist return err otherwise return email
+// async function addnewEmail(raw_email) {
+// try {
+//   email = new Email(raw_email);
+//   await email.save();
+//   return email;
+// } catch (err) {
+//   return false;
+// }
+// }
+
 // find all emails
-async function findAllEmails() {
-  return Email.find({}).exec();
-}
+// async function findAllEmails() {
+//   return Email.find({}).exec();
+// }
 
 // delete email and return true if delete
-async function deleteOneEmail(incomingEmail) {
-  let email = await Email.findOne({ email: incomingEmail }).exec();
-  if (!email) {
-    return false;
-  } else {
-    await Email.deleteOne({ email: incomingEmail }).exec();
-    return true;
-  }
-}
+// async function deleteOneEmail(incomingEmail) {
+//   let email = await Email.findOne({ email: incomingEmail }).exec();
+//   if (!email) {
+//     return false;
+//   } else {
+//     await Email.deleteOne({ email: incomingEmail }).exec();
+//     return true;
+//   }
+// }
 
 module.exports = {
-  addnewEmail,
-  findAllEmails,
-  deleteOneEmail,
+  // addnewEmail,
+  // findAllEmails,
+  // deleteOneEmail,
+  changeEmail,
 };
--- backend/routes/email.js
+++ backend/routes/email.js
@@ -3,29 +3,30 @@ const { body } = require("express-validator");
 const { isValidated } = require("../middleware/validation");
 const router = express.Router();
 const {
-  addnewEmail,
-  findAllEmails,
-  deleteOneEmail,
+  // addnewEmail,
+  // findAllEmails,
+  // deleteOneEmail,
+  changeEmail,
 } = require("../db/services/email");
 
-// @body: email, returns success:true if email is inserted
+// @body: email, returns success:true if email is changed
 router.post(
-  "/insert",
+  "/change",
   [body("email").notEmpty().isEmail(), isValidated],
   async (req, res, next) => {
     const { email } = req.body;
     try {
-      // try to add email and respond with err msg or success
+      // try to change email and respond with err msg or success
       emailJson = {
         email: email,
       };
-      const emailSuccessful = await addnewEmail(emailJson);
+      const emailSuccessful = await changeEmail(emailJson);
       if (!emailSuccessful) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "Enter a non-duplicate email" }] });
+          .json({ errors: [{ msg: "Email change unsuccessful" }] });
       } else {
-        return res.status(200).json({ sucess: true });
+        return res.status(200).json({ success: true });
       }
     } catch (err) {
       console.error(err.message);
@@ -35,39 +36,65 @@ router.post(
 );
 
 // @body: email, returns success:true if email is inserted
-router.delete(
-  "/remove",
-  [body("email").notEmpty().isEmail(), isValidated],
-  async (req, res, next) => {
-    const { email } = req.body;
-    try {
-      // check if user exists
-      const deleteSuccessful = await deleteOneEmail(email);
-      if (!deleteSuccessful) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Enter a valid email" }] });
-      } else {
-        return res.status(200).json({ sucess: true });
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server err");
-    }
-  }
-);
+// router.post(
+//   "/insert",
+//   [body("email").notEmpty().isEmail(), isValidated],
+//   async (req, res, next) => {
+//     const { email } = req.body;
+//     try {
+//       // try to add email and respond with err msg or success
+//       emailJson = {
+//         email: email,
+//       };
+//       const emailSuccessful = await addnewEmail(emailJson);
+//       if (!emailSuccessful) {
+//         return res
+//           .status(400)
+//           .json({ errors: [{ msg: "Enter a non-duplicate email" }] });
+//       } else {
+//         return res.status(200).json({ success: true });
+//       }
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send("Server err");
+//     }
+//   }
+// );
 
-router.get("/all", async (req, res, next) => {
-  try {
-    // returns emails or error if there is an error
-    const emails = await findAllEmails();
-    res.status(200).json({
-      emails: emails,
-    });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server err");
-  }
-});
+// @body: email, returns success:true if email is inserted
+// router.delete(
+//   "/remove",
+//   [body("email").notEmpty().isEmail(), isValidated],
+//   async (req, res, next) => {
+//     const { email } = req.body;
+//     try {
+//       // check if user exists
+//       const deleteSuccessful = await deleteOneEmail(email);
+//       if (!deleteSuccessful) {
+//         return res
+//           .status(400)
+//           .json({ errors: [{ msg: "Enter a valid email" }] });
+//       } else {
+//         return res.status(200).json({ success: true });
+//       }
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send("Server err");
+//     }
+//   }
+// );
+
+// router.get("/all", async (req, res, next) => {
+//   try {
+//     // returns emails or error if there is an error
+//     const emails = await findAllEmails();
+//     res.status(200).json({
+//       emails: emails,
+//     });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server err");
+//   }
+// });
 
 module.exports = router;
