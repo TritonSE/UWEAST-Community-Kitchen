652d32ba48840b47c1cb92168668caaadba3307f
Thomas Garry <32282600+thomas-gg@users.noreply.github.com>
2021-01-16 21:33:39 -0800

    fixed schema and commented

--- backend/db/models/order.js
+++ backend/db/models/order.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  Customer: {
-    Name: { type: String, required: true },
-    Email: { type: String, required: true },
-    Phone: { type: String, required: true },
+const orderSchema = new mongoose.Schema(
+  {
+    Customer: {
+      Name: { type: String, required: true },
+      Email: { type: String, required: true },
+      Phone: { type: String, required: true },
+    },
+    Pickup: { type: Date, required: true },
+    PayPal: {
+      Amount: { type: String, required: true },
+      transactionID: { type: String, required: true },
+    },
+    Order: [{ item: String, quantity: Number, extra: [String] }],
   },
-  Pickup: { type: Date, required: true },
-  Timestamps: { type: Boolean, required: true },
-  PayPal: {
-    Amount: { type: String, required: true },
-    transactionID: { type: String, required: true },
-  },
-  Order: [{ item: String, quantity: Number, extra: [String] }],
-});
+  { timestamps: true }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 module.exports = { Order };
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -8,41 +8,41 @@ const { addOrder, findOrders } = require("../db/services/order");
 // @body: PayPal(with Amount and transactionID),
 // @body: Order (array of objects with item string, quantity and extra array),
 // returns success:true if order is changed
-router.post(
-  "/insert",
-  [
-    body("Customer").notEmpty(),
-    body("Pickup").notEmpty(),
-    body("PayPal").notEmpty(),
-    body("Timestamps").notEmpty(),
-    body("Order").notEmpty(),
-    isValidated,
-  ],
-  async (req, res, next) => {
-    const { Customer, Pickup, Timestamps, PayPal, Order } = req.body;
-    try {
-      // try to add the order and respond with err msg or success
-      orderJson = {
-        Customer: Customer,
-        Pickup: Pickup,
-        Timestamps: Timestamps,
-        PayPal: PayPal,
-        Order: Order,
-      };
-      const orderSuccessful = await addOrder(orderJson);
-      if (!orderSuccessful) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Order unsuccessful" }] });
-      } else {
-        return res.status(200).json({ success: true });
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server err");
-    }
-  }
-);
+// router.post(
+//   "/insert",
+//   [
+//     body("Customer").notEmpty(),
+//     body("Pickup").notEmpty(),
+//     body("PayPal").notEmpty(),
+//     body("Timestamps").notEmpty(),
+//     body("Order").notEmpty(),
+//     isValidated,
+//   ],
+//   async (req, res, next) => {
+//     const { Customer, Pickup, Timestamps, PayPal, Order } = req.body;
+//     try {
+//       // try to add the order and respond with err msg or success
+//       orderJson = {
+//         Customer: Customer,
+//         Pickup: Pickup,
+//         Timestamps: Timestamps,
+//         PayPal: PayPal,
+//         Order: Order,
+//       };
+//       const orderSuccessful = await addOrder(orderJson);
+//       if (!orderSuccessful) {
+//         return res
+//           .status(400)
+//           .json({ errors: [{ msg: "Order unsuccessful" }] });
+//       } else {
+//         return res.status(200).json({ success: true });
+//       }
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send("Server err");
+//     }
+//   }
+// );
 
 // @body: startDate, endDate
 // returns emails between the given dates
