12073096e100095b039d356b216364d0b70491e1
Thomas Garry <32282600+thomas-gg@users.noreply.github.com>
2021-01-23 11:30:59 -0800

    refactoring of schema/routes

--- backend/db/models/item.js
+++ backend/db/models/item.js
@@ -1,21 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-  id: String,
-  name: String,
-  description: String,
-  price: Number,
-  category: String,
-  image: String,
-  cuisine: String,
-  tags: [String],
-  vegan: Boolean,
-  vegetarian: Boolean,
-  glutenFree: Boolean,
-  ingredients: [String],
-  completed: Boolean,
-  featured: Boolean,
+  Name: { type: String, required: true },
+  pictureURL: { type: String, required: true },
+  Description: { type: String, required: true },
+  Category: { type: String, required: true },
+  Prices: {
+    Individual: { type: String },
+    Family: { type: String },
+  },
+  isFeatured: { type: Boolean, default: false },
+  isCompleted: { type: Boolean, default: false },
+  Accomodations: {
+    type: [
+      {
+        Description: String,
+        Price: String,
+      },
+    ],
+    default: [],
+  },
+  // tags: { type: [String], required: true },
 });
-const Item = mongoose.model('Item', itemSchema);
+const Item = mongoose.model("Item", itemSchema);
 
 module.exports = { Item };
--- backend/db/services/item.js
+++ backend/db/services/item.js
@@ -1,60 +1,80 @@
-const mongoose = require('mongoose');
-const mongodb = require('mongodb');
-const { Item } = require('../models/item');
+const mongoose = require("mongoose");
+const mongodb = require("mongodb");
+const { Item } = require("../models/item");
 
 //Example of how to write queries/updates: https://github.com/TritonSE/tse-recruitment-backend/blob/master/services/applications.js
 
-
-function getAllMenuItems() {
-  return Item.find({}).exec();
+async function getAllMenuItems() {
+  try {
+    return Item.find({}).exec();
+  } catch (err) {
+    return false;
+  }
 }
 
-function itemFromInfo(info) {
-  return {
-    name: info.name,
-    description: info.description,
-    price: info.price,
-    category: info.category,
-    image: info.image,
-    cuisine: info.cuisine,
-    tags: info.tags,
-    vegan: info.vegan,
-    vegetarian: info.vegetarian,
-    glutenFree: info.glutenFree,
-    ingredients: info.ingredients,
-  };
+// function itemFromInfo(info) {
+//   return {
+//     name: info.name,
+//     description: info.description,
+//     price: info.price,
+//     category: info.category,
+//     image: info.image,
+//     cuisine: info.cuisine,
+//     tags: info.tags,
+//     vegan: info.vegan,
+//     vegetarian: info.vegetarian,
+//     glutenFree: info.glutenFree,
+//     ingredients: info.ingredients,
+//   };
+// }
+
+async function addNewItem(info) {
+  try {
+    return Item.create(info);
+  } catch (err) {
+    return false;
+  }
 }
 
-function addNewItem(info) {
-  Item.create(itemFromInfo(info));
+async function deleteItem(id) {
+  return await Item.deleteOne({ _id: new mongodb.ObjectID(id) }).exec();
 }
 
-function deleteItem(id) {
-  Item.deleteOne({ _id: new mongodb.ObjectID(id) }, (err, results) => {});
+async function editItem(id, info) {
+  try {
+    return await Item.updateOne(
+      { _id: new mongodb.ObjectID(id) },
+      { $set: info }
+    ).exec();
+  } catch (err) {
+    return false;
+  }
 }
 
-function editItem(id, info) {
-  Item.updateOne({ _id: new mongodb.ObjectID(id) },
-    { $set: itemFromInfo(info) },
-    (err, results) => {});
+async function setFeatured(id, featured) {
+  try {
+    return await Item.updateOne(
+      { _id: new mongodb.ObjectID(id) },
+      { $set: { isFeatured: featured } }
+    ).exec();
+  } catch (err) {
+    return false;
+  }
 }
 
-function setFeatured(id) {
-  Item.updateOne({ _id: new mongodb.ObjectID(id) },
-    { $set: { featured: true } },
-    (err, results) => {});
-}
+// function setNotFeatured(id) {
+//   Item.updateOne(
+//     { _id: new mongodb.ObjectID(id) },
+//     { $set: { featured: false } },
+//     (err, results) => {}
+//   );
+// }
 
-function setNotFeatured(id) {
-  Item.updateOne({ _id: new mongodb.ObjectID(id) },
-    { $set: { featured: false } },
-    (err, results) => {});
-}
-
-module.exports = { 
-    getAllMenuItems,
-    addNewItem,
-    deleteItem,
-    editItem,
-    setFeatured,
-    setNotFeatured };
+module.exports = {
+  getAllMenuItems,
+  addNewItem,
+  deleteItem,
+  editItem,
+  setFeatured,
+  // setNotFeatured,
+};
