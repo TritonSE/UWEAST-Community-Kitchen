9f7a891fdc27f4c18f835a7a39d35bc7bab07ce9
Navid Boloorian <navidboloorian0@gmail.com>
2021-01-31 14:07:31 -0800

    Fix for price overlapping title

--- frontend/src/components/MenuItem.js
+++ frontend/src/components/MenuItem.js
@@ -11,6 +11,8 @@ const MenuItem = ( {description, image, price, title, togglePopup, dietaryInfo} 
         <div className="menu-text">
           <h3 className="menu-name">{title}</h3>
           <p className="menu-description">{description}</p>
+        </div>
+        <div className="menu-absolutes">
           <p className="menu-price">${price}</p>
           <div className="menu-plus-button"><span className="menu-plus">+</span></div>
         </div>
