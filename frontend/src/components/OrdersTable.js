c9207b474044a20ee249c5d8270a78acc4262c83
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2021-02-05 04:13:54 -0800

    Finished the orders status for the orders page

--- frontend/src/components/OrdersTable.js
+++ frontend/src/components/OrdersTable.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-//Datatable
+import React from 'react';
 import MUIDataTable from "mui-datatables";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -8,12 +7,15 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 import DateRangePicker from 'react-bootstrap-daterangepicker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-daterangepicker/daterangepicker.css';
 import '../css/Orders.css';
-
-import OrdersStatus from './OrdersStatus';
 
 import {
   createMuiTheme,
@@ -35,6 +37,8 @@ function createData(name, description, size, quantity) {
 const renderRow = (rowData, rowMeta) => {
     const rows = []
     const length = rowData[5].length;
+
+    //Format the row information
     for(let i = 0; i < length; i++) {
       rows.push(createData(rowData[5][i].item, rowData[5][i].extra, rowData[5][i].size, rowData[5][i].quantity));
     }
@@ -104,6 +108,40 @@ const updateDateFilters = (filterList, filterPos, index) => {
 }
 
 /**
+ * This renders the select dropdown in the filter dropdown
+ * 
+ * @param {Object} filterList 
+ * @param {Function} onChange 
+ * @param {Index} index 
+ * @param {Array} column 
+ */
+const DisplayStatusFilters = (filterList, onChange, index, column) => {
+  
+  const filterStatus = (event, picker) => {
+    filterList[index][0] = event.target.value;
+    //Update the filter 
+    onChange(filterList[index], index, column);
+  } 
+  
+  return (
+    <FormControl >
+      <InputLabel id="demo-simple-select-label">Order Status</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={filterList[index][0] ? filterList[index][0] : "All Orders"}
+        onChange={filterStatus}
+      >
+        {/* All possible options */}
+        <MenuItem value={"All Orders"}>All Orders</MenuItem>
+        <MenuItem value={"Pending Orders"}>Pending Orders</MenuItem>
+        <MenuItem value={"Completed Orders"}>Completed Orders</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+/**
  * This function displays the date range picker when clicking on the
  * textfield inside the filters modal
  * @param {List} filterList 
@@ -123,8 +161,7 @@ const DisplayDateFilters = (filterList, onChange, index, column) => {
   //Called when the "apply" button is clicked
   const saveDate = (event, picker) => {
     const split = event.target.value.split(" - ");
-    filterList[index][0] = split[0];
-    filterList[index][1] = split[1];
+    filterList[index] = [split[0], split[1]];
     onChange(filterList[index], index, column);
   }
 
@@ -142,142 +179,67 @@ const DisplayDateFilters = (filterList, onChange, index, column) => {
 }
 
 /**
- * This renders the status dropdown
+ * This is called when the user changes the status of the 
+ * order 
+ * 
+ * @param {String} value 
+ * @param {Object} tableMeta 
+ * @param {Function} updateValue 
+ * @param {Object} e 
+ */
+const updateStatus = (value, tableMeta, updateValue, e) => {
+  const config = require('../config');
+  const BACKEND_URL = config.backend.uri;
+  
+  const setValue = (value === "Completed Orders") ? "Pending Orders" : "Completed Orders";
+  const getRowId = tableMeta.rowData[8];
+  
+  const requestBody = {
+    _id: getRowId,
+    isCompleted: (value === "Completed Orders") ? false : true
+  }
+
+  fetch(`${BACKEND_URL}order/updatestatus`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  }).then(async result => {
+      if (result.ok) console.log(result.statusText);
+      updateValue(setValue, tableMeta.rowIndex);
+    })
+    .catch(e => {
+        console.log(e);
+    });
+}
+
+/**
+ * This renders the Order status dropdown
  * 
  * @param {boolean} value - The value to pass in
  * @param {object} tableMeta - Information about the row
  */
 const renderStatus = (value, tableMeta, updateValue) => {
   return (
-    <OrdersStatus value={value} rowInfo={tableMeta} />
+    <div className="orders-status">
+        <select className={(value === "Completed Orders") ? "dropdown-menu-completed" : "dropdown-menu-pending"} value={value} 
+        onChange={(e) => updateStatus(value, tableMeta, updateValue, e)}
+            onClick={(e) => {
+                //This prevents the current row from expanding
+                e.stopPropagation();
+            }}>
+            <option value={"Pending Orders"}>Pending</option>
+            <option value={"Completed Orders"}>Completed</option>
+        </select> 
+    </div>  
   )
 }
-
-//The column headers for the table
-const columns = [
-{
-  name: "Pick up Details",
-  options: {
-    filter: true,
-    filterType: 'custom',
-    customFilterListOptions: {
-      render: renderDateFilters,
-      update: updateDateFilters
-    },
-    filterOptions: {
-      names: [],
-      logic(date, filters) {
-        const getDate = date.split("\n")[0];
-        if (filters[0] && filters[1]) {
-          return getDate < filters[0] || getDate > filters[1];
-        } else if (filters[0]) {
-          return getDate < filters[0];
-        } else if (filters[1]) {
-          return getDate > filters[1];
-        }
-        return false;
-      },
-      display: DisplayDateFilters
-    }
-  }  
-},
-{
-  name: "Name",
-  options: {
-    filter: true,
-    filterType: 'textField'
-  }  
-},
-{
-  name: "Email",
-  options: {
-    filter: true,
-    filterType: 'textField'
-  }    
-},
-{
-  name: "Phone Number",
-  options: {
-    filter: true,
-    filterType: 'textField'
-  }  
-},
-{
-  name: "Amount Paid",
-  options: {
-    filter: false
-  }  
-},
-{
-  name: "Order Description",
-  options: {
-    display: false, 
-    viewColumns: false, 
-    filter: false
-  }
-},
-{
-  name: "Submission Details",
-  options: {
-    filter: true,
-    filterType: 'custom',
-    customFilterListOptions: {
-      render: renderDateFilters,
-      update: updateDateFilters
-    },
-    filterOptions: {
-      names: [],
-      logic(date, filters) {
-        const getDate = date.split("\n")[0];
-        if (filters[0] && filters[1]) {
-          return getDate < filters[0] || getDate > filters[1];
-        } else if (filters[0]) {
-          return getDate < filters[0];
-        } else if (filters[1]) {
-          return getDate > filters[1];
-        }
-        return false;
-      },
-      display: DisplayDateFilters
-    }
-  }  
-},
-{
-  name: "Order Status",
-  options: {
-    filter: true,
-    filterType: 'dropdown',
-    customBodyRender: renderStatus,
-    filterOptions: {
-      names: ["Pending Orders", "Completed Orders"],
-      logic(date, filters) {
-
-        if (filters[0] === "Completed Orders") {
-          console.log('here');
-          return date === false;
-        } else if (filters[0] === "Pending Orders") {
-          return date === true;
-        }
-        return false;
-      },
-    }
-  }  
-},
-{
-  name: "Row ID",
-  options: {
-    display: false, 
-    viewColumns: false, 
-    filter: false
-  }
-}
-];
 
 export default function OrdersTable(props) {
   
   const options = {
     filter: true,
-    filterType: 'textField',
     expandableRowsOnClick: true,
     expandableRows: true,
     selectableRows: 'none',
@@ -285,6 +247,132 @@ export default function OrdersTable(props) {
     renderExpandableRow: renderRow,
     searchOpen: true,
   };
+
+  //The column headers for the table
+const columns = [
+  {
+    name: "Pick up Details",
+    options: {
+      filter: true,
+      filterType: 'custom',
+      customFilterListOptions: {
+        render: renderDateFilters,
+        update: updateDateFilters
+      },
+      filterOptions: {
+        names: [],
+        logic(date, filters) {
+          const getDate = date.split("\n")[0];
+          if (filters[0] && filters[1]) {
+            return getDate < filters[0] || getDate > filters[1];
+          } else if (filters[0]) {
+            return getDate < filters[0];
+          } else if (filters[1]) {
+            return getDate > filters[1];
+          }
+          return false;
+        },
+        display: DisplayDateFilters
+      }
+    }  
+  },
+  {
+    name: "Name",
+    options: {
+      filter: true,
+      filterType: 'textField'
+    }  
+  },
+  {
+    name: "Email",
+    options: {
+      filter: true,
+      filterType: 'textField'
+    }    
+  },
+  {
+    name: "Phone Number",
+    options: {
+      filter: true,
+      filterType: 'textField'
+    }  
+  },
+  {
+    name: "Amount Paid",
+    options: {
+      filter: false
+    }  
+  },
+  {
+    name: "Order Description",
+    options: {
+      display: false, 
+      viewColumns: false, 
+      filter: false
+    }
+  },
+  {
+    name: "Submission Details",
+    options: {
+      filter: true,
+      filterType: 'custom',
+      customFilterListOptions: {
+        render: renderDateFilters,
+        update: updateDateFilters
+      },
+      filterOptions: {
+        names: [],
+        logic(date, filters) {
+          const getDate = date.split("\n")[0];
+          if (filters[0] && filters[1]) {
+            return getDate < filters[0] || getDate > filters[1];
+          } else if (filters[0]) {
+            return getDate < filters[0];
+          } else if (filters[1]) {
+            return getDate > filters[1];
+          }
+          return false;
+        },
+        display: DisplayDateFilters
+      }
+    }  
+  },
+  {
+    name: "Order Status",
+    options: {
+      filter: true,
+      filterType: 'custom',
+      customBodyRender: renderStatus,
+      customFilterListOptions: {
+        render: (options) => {
+          if(options[0] === "All Orders") return [];
+          return options;
+        },
+      },
+      filterOptions: {
+        names: ["Pending Orders", "Completed Orders"],
+        logic(order, filters) {
+          if (filters[0] == "Completed Orders") {
+            return order === "Pending Orders";
+          } else if (filters[0] == "Pending Orders") {
+            return order === "Completed Orders";
+          }
+
+          return false;
+        },
+        display: DisplayStatusFilters
+      }
+    }
+  },
+  {
+    name: "Row ID",
+    options: {
+      display: false, 
+      viewColumns: false, 
+      filter: false
+    }
+  }
+  ];
 
   const getMuiTheme = () =>
   createMuiTheme({
@@ -310,7 +398,6 @@ export default function OrdersTable(props) {
     },
   });
 
-  console.log(props.orders);
   return (
       <MuiThemeProvider theme={getMuiTheme()}>
         <MUIDataTable
--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -2,7 +2,6 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import OrdersTable from '../components/OrdersTable';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { makeStyles } from '@material-ui/core/styles';
 import '../css/Orders.css';
 
 const config = require('../config');
@@ -20,8 +19,32 @@ export default class Orders extends React.Component {
 
         this.formatTime = this.formatTime.bind(this);
         this.formatDate = this.formatDate.bind(this);
+        this.formatArray = this.formatArray.bind(this);
     }
 
+    /**
+     * Formats the data from the fetch() call into the array
+     * @param {Object} list 
+     * @param {String} date 
+     * @param {String} formatCurrTime 
+     * @param {String} dateSubmission 
+     * @param {String} formatCurrTimeSubmission 
+     */
+    formatArray(list, date, formatCurrTime, dateSubmission, formatCurrTimeSubmission) {
+        let val = formatCurrTime;
+        let val2 = formatCurrTimeSubmission;
+
+        const dateOne = date + `\n${val}`;
+        const dateTwo = dateSubmission + `\n${val2}`;
+        return [dateOne, list.Customer.Name, list.Customer.Email, list.Customer.Phone, 
+            list.PayPal.Amount, list.Order, dateTwo, list.isCompleted ? "Completed Orders" : "Pending Orders", 
+            list._id];
+    }
+
+    /**
+     * Formats the time in the HH:MM (P.M. OR A.M.)
+     * @param {String} time 
+     */
     formatTime(time) {
         time = time.split(':'); // convert to array
         
@@ -31,7 +54,7 @@ export default class Orders extends React.Component {
 
         if (hours > 0 && hours <= 12) timeValue = "" + hours; 
         else if (hours > 12) timeValue = "" + (hours - 12);
-        else if (hours == 0) timeValue = "12";
+        else if (hours === 0) timeValue = "12";
         
         timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
         timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
@@ -39,12 +62,19 @@ export default class Orders extends React.Component {
         return timeValue;
     }
 
+    /**
+     * Formats the date to be in the MM/DD/YYYY format
+     * @param {String} getDate 
+     */
     formatDate(getDate) {
         const monthSubmission = getDate.getMonth()+1 >= 10 ? getDate.getMonth()+1 : ("0" + (getDate.getMonth() + 1)).slice(-2);
         const dateSubmission = monthSubmission + "/" + getDate.getDate() + "/" + getDate.getFullYear();   
         return dateSubmission 
     }
 
+    /**
+     * Get all the orders from the database
+     */
     componentDidMount() {
         fetch(`${BACKEND_URL}order`, {
             method: 'POST',
@@ -54,12 +84,10 @@ export default class Orders extends React.Component {
         }).then(res => res.json())
         .then(data => {
             const getOrdersList = data.orders;
-            console.log(getOrdersList);
             const length = getOrdersList.length
             let createArr = [];
 
             for(let i = 0; i < length; i++) {
-                let arr = [];
                 //Get the date from the database
                 let getDate = new Date(getOrdersList[i].Pickup);
                 const formatCurrtime = this.formatTime(getDate.getHours() + ":" + getDate.getMinutes() + ":" + getDate.getSeconds());
@@ -70,17 +98,8 @@ export default class Orders extends React.Component {
                 const formatCurrtimeSubmission = this.formatTime(getDateSubmission.getHours() + ":" + getDateSubmission.getMinutes() + ":" + getDateSubmission.getSeconds());
                 const dateSubmission = this.formatDate(getDateSubmission);
 
-                arr.push(date + `\n${formatCurrtime}`);
-                arr.push(getOrdersList[i].Customer.Name);
-                arr.push(getOrdersList[i].Customer.Email);
-                arr.push(getOrdersList[i].Customer.Phone);
-                arr.push(getOrdersList[i].PayPal.Amount);
-                arr.push(getOrdersList[i].Order);
-                arr.push(dateSubmission + `\n${formatCurrtimeSubmission}`);
-                arr.push(getOrdersList[i].isCompleted);
-                arr.push(getOrdersList[i]._id);
-
-                createArr.push(arr);
+                const formatedArray = this.formatArray(getOrdersList[i], date, formatCurrtime, dateSubmission, formatCurrtimeSubmission)
+                createArr.push(formatedArray);
             }
             this.setState({ getOrders: createArr, Loading: false})
         })
@@ -91,6 +110,7 @@ export default class Orders extends React.Component {
         return (
             <div className="orders-page-container">
                 <NavBar />
+                {/* Render a progress spinner to show it is loading */}
                 {this.state.Loading ? 
                 <div className="spinner-orders-page">
                     <CircularProgress />
