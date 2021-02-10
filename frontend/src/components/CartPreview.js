f5eb803ef85b3077627847e1a0ab9b1824cff380
Dhanush <dhanunanjunda2@gmail.com>
2021-02-09 20:10:03 -0800

    change description to size, accommodations, and special instructions and added sales tax

--- frontend/src/components/CartPreview.js
+++ frontend/src/components/CartPreview.js
@@ -23,14 +23,20 @@ class CartPreview extends Component {
 
                     this.state.subTotal = parseFloat(this.state.subTotal) + parseFloat(item.price);
                     this.state.subTotal = parseFloat(this.state.subTotal).toFixed(2);
+                    this.state.tax = (parseFloat(this.state.subTotal)*0.0775).toFixed(2);
                     this.state.totalPrice = parseFloat(this.state.subTotal) + parseFloat(this.state.tax);
                     this.state.totalPrice = parseFloat(this.state.totalPrice).toFixed(2);
+
+                    let specialInstructions = (item.instructions === "") ? "" : ", " + item.instructions;
+                    let accommodation = (item.accommodations) ? ", " + item.accommodations : "";
+
+                    let extraInfo = item.size + specialInstructions  + accommodation;
 
                         return (
                             <div key={ind} className="summary-item row">
                                 <span className="thumbnail thumb-img">{ind+1}</span>
                                 <span className="item-info">{item.quantity} X {item.name}<br/>
-                                <span className="item-description">{item.description}</span></span>
+                                <span className="item-description">{extraInfo}</span></span>
                                 <span className="thumbnail summary-price">${item.price}</span>
                                 <span className="item-divider"></span>
                             </div>
