6829651c9ba60267a470d456e5ba996824399b45
Thomas Garry <32282600+thomas-gg@users.noreply.github.com>
2021-02-14 22:34:38 -0800

    initial updated routes and services

--- backend/db/services/email.js
+++ backend/db/services/email.js
@@ -1,8 +1,9 @@
 const { Email } = require("../models/email");
 
-// change email
-async function changeEmail(raw_email) {
+// change the primary email in the DB
+async function changePrimaryEmail(raw_email) {
   try {
+    // find and replace the primary email
     let email = await Email.findOneAndUpdate({}, raw_email, { new: true });
     // if there are no emails in database insert one
     if (email === null) {
@@ -15,36 +16,46 @@ async function changeEmail(raw_email) {
   }
 }
 
-// if email doesn't exist return err otherwise return email
-// async function addnewEmail(raw_email) {
-// try {
-//   email = new Email(raw_email);
-//   await email.save();
-//   return email;
-// } catch (err) {
-//   return false;
-// }
-// }
-
-// find all emails
-async function findAllEmails() {
-  return Email.find({}).exec();
+// add secondary email if it doesn't already exist
+async function addSecondaryEmail(raw_email) {
+  try {
+    let email = await Email.findOne(raw_email).exec();
+    // if there are no secondary emails with this name in the database insert one
+    if (email === null) {
+      email = new Email(raw_email);
+      await email.save();
+    }
+    return email;
+  } catch (err) {
+    return false;
+  }
 }
 
-// delete email and return true if delete
-// async function deleteOneEmail(incomingEmail) {
-//   let email = await Email.findOne({ email: incomingEmail }).exec();
-//   if (!email) {
-//     return false;
-//   } else {
-//     await Email.deleteOne({ email: incomingEmail }).exec();
-//     return true;
-//   }
-// }
+// finds the primary email in the DB
+async function findPrimaryEmail() {
+  return Email.findOne({ isPrimary: true }).exec();
+}
+
+// find all emails
+async function findAllSecondaryEmails() {
+  return Email.find({ isPrimary: false }).exec();
+}
+
+// delete email and return true if deleted
+async function deleteSecondaryEmail(incomingEmail) {
+  let email = await Email.findOne({ email: incomingEmail }).exec();
+  if (!email) {
+    return false;
+  } else {
+    await Email.deleteOne({ email: incomingEmail }).exec();
+    return true;
+  }
+}
 
 module.exports = {
-  // addnewEmail,
-  findAllEmails,
-  // deleteOneEmail,
-  changeEmail,
+  findPrimaryEmail,
+  findAllSecondaryEmails,
+  deleteSecondaryEmail,
+  changePrimaryEmail,
+  addSecondaryEmail,
 };
--- backend/routes/email.js
+++ backend/routes/email.js
@@ -3,15 +3,16 @@ const { body } = require("express-validator");
 const { isValidated } = require("../middleware/validation");
 const router = express.Router();
 const {
-  // addnewEmail,
-  findAllEmails,
-  // deleteOneEmail,
-  changeEmail,
+  findPrimaryEmail,
+  findAllSecondaryEmails,
+  deleteSecondaryEmail,
+  changePrimaryEmail,
+  addSecondaryEmail,
 } = require("../db/services/email");
 
 // @body: email, returns success:true if email is changed
 router.post(
-  "/change",
+  "/changePrimary",
   [body("email").notEmpty().isEmail(), isValidated],
   async (req, res, next) => {
     const { email } = req.body;
@@ -19,8 +20,9 @@ router.post(
       // try to change email and respond with err msg or success
       emailJson = {
         email: email,
+        isPrimary: true,
       };
-      const emailSuccessful = await changeEmail(emailJson);
+      const emailSuccessful = await changePrimaryEmail(emailJson);
       if (!emailSuccessful) {
         return res
           .status(400)
@@ -35,61 +37,77 @@ router.post(
   }
 );
 
-// @body: email, returns success:true if email is inserted
-// router.post(
-//   "/insert",
-//   [body("email").notEmpty().isEmail(), isValidated],
-//   async (req, res, next) => {
-//     const { email } = req.body;
-//     try {
-//       // try to add email and respond with err msg or success
-//       emailJson = {
-//         email: email,
-//       };
-//       const emailSuccessful = await addnewEmail(emailJson);
-//       if (!emailSuccessful) {
-//         return res
-//           .status(400)
-//           .json({ errors: [{ msg: "Enter a non-duplicate email" }] });
-//       } else {
-//         return res.status(200).json({ success: true });
-//       }
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send("Server err");
-//     }
-//   }
-// );
+// @body: email, returns success:true if email is changed
+router.post(
+  "/addSecondary",
+  [body("email").notEmpty().isEmail(), isValidated],
+  async (req, res, next) => {
+    const { email } = req.body;
+    try {
+      // try to change email and respond with err msg or success
+      emailJson = {
+        email: email,
+        isPrimary: false,
+      };
+      const emailSuccessful = await addSecondaryEmail(emailJson);
+      if (!emailSuccessful) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Email change unsuccessful" }] });
+      } else {
+        return res.status(200).json({ success: true });
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server err");
+    }
+  }
+);
 
-// @body: email, returns success:true if email is inserted
-// router.delete(
-//   "/remove",
-//   [body("email").notEmpty().isEmail(), isValidated],
-//   async (req, res, next) => {
-//     const { email } = req.body;
-//     try {
-//       // check if user exists
-//       const deleteSuccessful = await deleteOneEmail(email);
-//       if (!deleteSuccessful) {
-//         return res
-//           .status(400)
-//           .json({ errors: [{ msg: "Enter a valid email" }] });
-//       } else {
-//         return res.status(200).json({ success: true });
-//       }
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send("Server err");
-//     }
-//   }
-// );
+// @body: email, returns success:true if the email is deleted
+router.delete(
+  "/removeSecondary",
+  [body("email").notEmpty().isEmail(), isValidated],
+  async (req, res, next) => {
+    const { email } = req.body;
+    try {
+      // check if user exists
+      const deleteSuccessful = await deleteSecondaryEmail(email);
+      if (!deleteSuccessful) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Enter a valid secondary email" }] });
+      } else {
+        return res.status(200).json({ success: true });
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server err");
+    }
+  }
+);
 
-router.get("/all", async (req, res, next) => {
+// returns an array of all the secondary emails in the DB
+router.get("/secondary", async (req, res, next) => {
   try {
     // returns emails or error if there is an error
-    const emails = await findAllEmails();
+    const emails = await findAllSecondaryEmails();
     res.status(200).json({
       emails: emails,
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server err");
+  }
+});
+
+// returns the primary email in the DB
+router.get("/primary", async (req, res, next) => {
+  try {
+    // returns emails or error if there is an error
+    const email = await findPrimaryEmail();
+    res.status(200).json({
+      email: email,
     });
   } catch (err) {
     console.error(err.message);
