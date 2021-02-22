3b543146ff4a9e0bd6a0215b709ccc49285a0bca
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2021-02-21 23:33:00 -0800

    fixed changes required

--- frontend/src/components/ChangeEmailScreen.js
+++ frontend/src/components/ChangeEmailScreen.js
@@ -59,6 +59,13 @@ export default function ChangeEmailScreen (props) {
             setErrorMessage("This email is already listed as a secondary email.");  
             return;
         }
+
+        // check for empty string
+        if(email.length === 0) {
+            setInputError(true);
+            setErrorMessage("Enter a valid email address.");    
+            return;
+        }
         
         // validate to make sure email is an email
         if (email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
