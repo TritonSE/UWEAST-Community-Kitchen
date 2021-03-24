269a24f895b992a068cfe27af340a3e0b3e835ee
Amrit-S <aksingh@ucsd.edu>
2021-03-23 18:54:35 -0700

    Added rejection emails for paypal ipn

--- backend/routes/paypal.js
+++ backend/routes/paypal.js
@@ -2,11 +2,37 @@ const express = require("express");
 var querystring = require('querystring');
 var request = require('request');
 const { updatePayPalStatus, findOrderByTid } = require("../db/services/order");
+const { findAllEmails } = require("../db/services/email");
 const { getAllMenuItems } = require("../db/services/item");
+const { sendEmail } = require("../routes/services/mailer");
 const config = require("../config");
 const router = express.Router();
 
 const TAX_RATE = 0.0775; 
+
+async function setUpEmail(data, res){
+  // retrieve all emails inside of Emails DB
+  const emails = await findAllEmails();
+  if (!emails.length) {
+    return res.status(400).json({ errors: [{ msg: "no emails found" }] });
+  }
+
+  dbemail = emails.map(function (item) {
+    return item.email;
+  });
+
+  let locals = {
+    transactionID: data.transactionID || "",
+    verified: data.verified || false,
+    reason: data.reason || "",
+    expected_amount: data.expected_amount || "",
+    actual_amount: data.actual_amount || ""
+  }
+
+  // send UWEAST a notification that an order has been rejected
+  sendEmail("uweast-order-rejection", dbemail, locals, res);
+
+}
 
 function getAccomPrice(acc, arr){
   for(var i = 0; i < arr.length; i++)
@@ -20,7 +46,7 @@ function getAccomPrice(acc, arr){
   throw 500;
 }
 
-async function priceValidation(order, payment_amount){
+async function priceValidation(order, payment_amount, txn_id, res){
   let menuItems = await getAllMenuItems();
    let dict_menuItems = {};
    for(var j = 0; j < menuItems.length; j++){ 
@@ -43,23 +69,26 @@ async function priceValidation(order, payment_amount){
 
       let item_price = parseFloat(item_object["Prices"][size]);
 
-      // console.log(`Price: ${item_price}`);
-      // console.log(accommodations);
       for(var k = 0; k < accommodations.length; k++){
 
         let accom_price = parseFloat(getAccomPrice(accommodations[k], item_object["Accommodations"]));
         item_price += accom_price;
-        // console.log(`${accommodations[k]}: ${accom_price}`);
-        // console.log(`Price: ${item_price}`);
       }
 
       item_price *= quantity;
-      // console.log(`Price: ${item_price} (x${quantity})`);
 
       expected_total += item_price;
      } catch(err){
-      // console.log("Rejected: Bad Price Verification");
+      //console.log("Rejected: Bad Price Verification");
       await updatePayPalStatus(order._id, 2);
+      let locals = {
+        transactionID: txn_id,
+        verified: true,
+        reason: "CONTENTS",
+        expected_amount: expected_total,
+        actual_amount: payment_amount
+      }
+      setUpEmail(locals,res);
       return true;
      }
 
@@ -68,12 +97,18 @@ async function priceValidation(order, payment_amount){
    expected_total += (expected_total * TAX_RATE)
    expected_total = expected_total.toFixed(2);
 
-  //  console.log("EXPECTED AMOUNT: " + expected_total);
-  //  console.log("ACTUAL AMOUNT: " + payment_amount);
 
    if(expected_total != payment_amount){
     // console.log("Rejected: Expected not equal to Total Paid");
     await updatePayPalStatus(order._id, 2);
+    let locals = {
+      transactionID: txn_id,
+      verified: true,
+      reason: "PRICE",
+      expected_amount: expected_total,
+      actual_amount: payment_amount
+    }
+    setUpEmail(locals,res);
     return true;
    }
 
@@ -81,52 +116,55 @@ async function priceValidation(order, payment_amount){
 }
 
 
-async function inspectIPNResponse(res){
-
-  console.log("Verified\n");
+async function inspectValidIPNResponse(req, res){
 
    // assign posted variables to local variables
-   var payment_status = res.body['payment_status'];
-   var txn_id = res.body['txn_id'];
-   var receiver_email = res.body['receiver_email'];
-   var payment_amount = res.body['mc_gross'];
+   var payment_status = req.body['payment_status'];
+   var txn_id = req.body['txn_id'];
+   var receiver_email = req.body['receiver_email'];
+   var payment_amount = req.body['mc_gross'];
 
    // only proceed if the transaction has been fully completed, and transaction comes from the right merchant
    if(payment_status != 'Completed'){
     //  console.log(payment_status);
-     console.log("Rejected: Status");
+     //console.log("Rejected: Status");
      return;
    }
 
    if(receiver_email != config.paypal.PAYPAL_ClIENT_EMAIL){
-    console.log("Rejected: Email");
+    //console.log("Rejected: Email");
     return;
   }
 
    // try to find the corresponding order in the database
    let order = await findOrderByTid(txn_id);
    if(!order){
-      console.log("Rejected: Order does not exist");
+      //console.log("Rejected: Order does not exist");
       return;
    }
 
-   console.log(order);
-
    // verify gross amount paid are same 
    if(payment_amount != order.PayPal.Amount){
-    console.log("Rejected: Mismatched paid amount");
+    //console.log("Rejected: Mismatched paid amount");
     await updatePayPalStatus(order._id, 2);
+    let locals = {
+      transactionID: txn_id,
+      verified: true,
+      reason: "MISMATCH",
+      expected_amount: order.PayPal.Amount,
+      actual_amount: payment_amount
+    }
+    setUpEmail(locals,res);
     return;
    }
 
    // verify order total is correct based off of items bought 
-   if(await priceValidation(order, payment_amount)){
+   if(await priceValidation(order, payment_amount, txn_id, res)){
      return;
    }
 
    // order already marked bad, return 
    if(order.PayPal.status === 2){
-     console.log("Rejected: Order has bad status");
      return;
    }
 
@@ -137,12 +175,27 @@ async function inspectIPNResponse(res){
 
 }
 
+async function inspectInvalidIPNResponse(req, res){
+     // try to find the corresponding order in the database
+     let txn_id = req.body['txn_id']; 
+     let order = await findOrderByTid(txn_id);
+     if(!order || order.PayPal.status === 2){
+
+         return;
+     }
+     await updatePayPalStatus(order._id, 2);
+     let locals = {
+       transactionID: txn_id,
+       verified: false
+     }
+     setUpEmail(locals,res);
+     // IPN invalid, log for manual investigation
+     console.error('Invalid IPN!'.error);
+}
+
 router.post(
   "/ipn-listener",
   async (req, res, next) => {
-    console.log('Received POST /'.bold);
-    console.log(req.body);
-    console.log("TRANSACTION ID: " + req.body.txn_id);
 
     res.sendStatus(200);
     res.end();
@@ -173,9 +226,6 @@ router.post(
       };
 
       // Step 2: POST IPN data back to PayPal to validate
-    console.log('Posting back to paypal'.bold);
-    console.log(payload);
-    console.log('\n\n');
     var options = {
       url: paypalURL,
       method: 'POST',
@@ -195,19 +245,16 @@ router.post(
         console.error("Could not make handshake request to PayPal");
         return;
       }
-      console.log(response.statusCode);
-      console.log(body);
 
       if (!error && response.statusCode === 200) {
   
         // inspect IPN validation result and act accordingly
         if (body.substring(0, 8) === 'VERIFIED') {
 
-            inspectIPNResponse(req);
+          inspectValidIPNResponse(req, res);
 
         } else if (body.substring(0, 7) === 'INVALID') {
-          // IPN invalid, log for manual investigation
-          console.error('Invalid IPN!'.error);
+            inspectInvalidIPNResponse(req, res)
         }
       }
     });
