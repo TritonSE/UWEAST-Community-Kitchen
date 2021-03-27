100e893654c16e7ef2df2b006fe81f5b7b24019b
Amrit-S <aksingh@ucsd.edu>
2021-03-27 08:12:52 -0700

    updated frontend env vars naming

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     env: process.env.NODE_ENV || 'development',
   },
   backend: {
-    uri: process.env.BACKEND_URI || 'http://localhost:9000/'
+    uri: process.env.REACT_APP_BACKEND_URI || 'http://localhost:9000/'
   },
   google: {
     MAPS_API_CODE: process.env.REACT_APP_MAPS_API_CODE || ""
