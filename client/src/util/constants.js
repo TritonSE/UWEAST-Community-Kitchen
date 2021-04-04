a4c57e8e1b5cd25a30e888db521fb1780f179c66
Amrit-S <aksingh@ucsd.edu>
2021-04-04 01:48:02 -0700

    min amount changed to 20

--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -9,5 +9,5 @@ export const CONTACT_PHONE_NUMBER = "(619) 831-7883";
 export const MIN_PICKUP_ELAPSE_DAYS = 3;
 
 // frontend + backend global constants - both backend & frontend constant files must be modified 
-export const MIN_CART_TOTAL_CHECKOUT = 1;
+export const MIN_CART_TOTAL_CHECKOUT = 20;
 export const ORDER_SERVICE_TAX_RATE = 0.0775;
